0dd6f7b2c0764516bf15518b4cce718f9ecb14de
kawagoetakumi <120728595+kawagoetakumi@users.noreply.github.com>
2023-07-22 17:13:34 +0900

    Create main.java

---
+++ 4-1/src/check/main.java
@@ -0,0 +1,29 @@
+package check;
+
+public class main {
+	private static String firstName = "川越";
+	private static String lastName = "拓巳";
+		// TODO 自動生成されたメソッド・スタブ
+
+	private static void printName() {
+		System.out.println("printNameメソッド" +"→" + firstName + lastName);
+	}
+	
+
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+	   printName();
+	   
+	   Pet Name2 = new Pet("CHECK_CLASS_JAVA", "CHECK_CLASS_HOGE"); 
+	   Name2.introduce();
+	   
+	   
+	  RobotPet Name3 = new RobotPet("CHECK_CLASS_R2D2", "CHECK_CLASS_LUKE");
+	    Name3.introduce();
+		   
+	  
+		 
+	}
+}
+		
