5b8d40ea14c612a31fad0990a8d39e67e7c5ab4b
kawagoetakumi <120728595+kawagoetakumi@users.noreply.github.com>
2023-07-26 23:31:42 +0900

    Update main.java

--- 4-1/src/check/main.java
+++ 4-1/src/check/main.java
@@ -1,5 +1,7 @@
 package check;
 
+import constants.Constants;
+
 public class main {
 	private static String firstName = "川越";
 	private static String lastName = "拓巳";
@@ -15,12 +17,14 @@ public class main {
 		// TODO 自動生成されたメソッド・スタブ
 	   printName();
 	   
-	   Pet Name2 = new Pet("CHECK_CLASS_JAVA", "CHECK_CLASS_HOGE"); 
+	   Pet Name2 = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
 	   Name2.introduce();
 	   
+	   RobotPet Name3 = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
+	   Name3.introduce();
 	   
-	  RobotPet Name3 = new RobotPet("CHECK_CLASS_R2D2", "CHECK_CLASS_LUKE");
-	    Name3.introduce();
+	
+	    
 		   
 	  
 		 
