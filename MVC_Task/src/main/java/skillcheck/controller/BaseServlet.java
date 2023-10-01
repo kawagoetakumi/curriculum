cacea7339ff5e88e01676f9b37e49d2619a48bfa
kawagoetakumi <120728595+kawagoetakumi@users.noreply.github.com>
2023-10-01 23:57:57 +0900

    Update BaseServlet.java

--- MVC_Task/src/main/java/skillcheck/controller/BaseServlet.java
+++ MVC_Task/src/main/java/skillcheck/controller/BaseServlet.java
@@ -11,6 +11,7 @@ import jakarta.servlet.http.HttpSession;
 import skillcheck.bean.EmployeeBean;
 import skillcheck.bean.ResponseBean;
 import skillcheck.constant.ConstMessage;
+import skillcheck.dao.EmployeeDao.ExecuteCase;
 import skillcheck.exception.MVCException;
 import skillcheck.logger.Logger;
 import skillcheck.service.EmployeeManagementService;
@@ -29,7 +30,7 @@ public abstract class BaseServlet extends HttpServlet {
 
     // FIXME Step-3: 定数定義
     // FIXME Step-3-1: リクエスト判別用のボタンの属性名を記述しなさい。
-    protected static final String CONST_ELEMENT_NAME_REQUEST = "request";
+    protected static final String CONST_ELEMENT_NAME_REQUEST = "requestTypeName";
     protected static final String CONST_REQUST_KEY_FOR_SENDER = "sender";
     protected static final String CONST_REQUST_KEY_FOR_REDIRECT = "redirect";
     protected static final String CONST_REQUST_KEY_FOR_RESPONSE_BEAN = "responseBean";
@@ -38,6 +39,8 @@ public abstract class BaseServlet extends HttpServlet {
     protected static final String CONST_DESTINATION_LOGIN_JSP = "/MVC_Task/login.jsp";
     // FIXME Step-3-2: 実行結果表示用のjspファイルのパスを記述しなさい。
     protected static final String CONST_DESTINATION_RESULT_JSP = "/MVC_Task/src/main/webapp/employeeResult.jsp";
+
+	private static final String FIND_BY_EMPID = null;
 
     /* フィールド変数の定義 */
     /** フォーワード先 */
@@ -148,9 +151,9 @@ public abstract class BaseServlet extends HttpServlet {
             // Tips1: 社員情報管理サービスはインスタンスが生成済みのものを利用すること
             // Tips2: 完全一致検索の社員情報取得を呼び出すこと
             // Tips3: 第二引数の渡し方に注意すること
-        	EmployeeBean Emp = new EmployeeBean();
-        	String id =request.getParameter(Emp , "FIND_BY_EMPID");
-
+        	EmployeeBean reqEmployeeBean = new EmployeeBean(reqEmpId);
+//        	EmployeeManagementService.getEmployeeData( EmployeeDaoFIND_BY_EMPID ,reqEmployeeBean );
+        	ems.getEmployeeData(ExecuteCase.FIND_BY_EMPID, reqEmployeeBean);
              
             // 最初の1件を取得
             resEmployeeBean = responseBean.getEmplyeeBeanList().stream().findFirst().orElse(null);
